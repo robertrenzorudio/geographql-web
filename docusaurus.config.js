ac5e4be6def859301829ae14f2afb7a38f60475b
Robert Renzo Rudio <robertrenzorudio@gmail.com>
2021-12-28 13:43:29 -0800

    update url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'GeographQL',
   tagline: 'A Country, State, and City GraphQL API',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://geographql.netlify.app',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
