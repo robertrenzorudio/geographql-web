f1ea903965d43cbf97d6c2b7ef8991d7cc9c690f
Robert Renzo Rudio <robertrenzorudio@gmail.com>
2022-01-03 20:21:06 -0800

    Change cursor pagination svg

--- src/components/HomepageFeatures.tsx
+++ src/components/HomepageFeatures.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import clsx from 'clsx';
 import styles from './HomepageFeatures.module.css';
-import useThemeContext from '@theme/hooks/useThemeContext';
 import { Link } from '@docusaurus/router';
 
 type FeatureItem = {
   title: string;
   image: string;
-  imageDark: string;
   description: JSX.Element;
 };
 
@@ -15,7 +13,6 @@ const FeatureList: FeatureItem[] = [
   {
     title: 'GraphQL Cursor-Based Pagination',
     image: '/img/cursor-pagination.svg',
-    imageDark: '/img/cursor-pagination-dark.svg',
     description: (
       <>
         The API uses cursor-based pagination conforming to the &nbsp;
@@ -28,7 +25,6 @@ const FeatureList: FeatureItem[] = [
   {
     title: 'Rate Limit',
     image: '/img/rate-limit.svg',
-    imageDark: '/img/rate-limit.svg',
     description: (
       <>
         The API uses point-based <Link to="/docs/rate-limit">rate-limit</Link>{' '}
@@ -38,16 +34,11 @@ const FeatureList: FeatureItem[] = [
   },
 ];
 
-function Feature({ title, image, imageDark, description }: FeatureItem) {
-  const { isDarkTheme } = useThemeContext();
+function Feature({ title, image, description }: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <img
-          className={styles.featureSvg}
-          alt={title}
-          src={isDarkTheme ? imageDark : image}
-        />
+        <img className={styles.featureSvg} alt={title} src={image} />
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
